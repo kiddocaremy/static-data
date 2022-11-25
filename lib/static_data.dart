99b0ad8ae5dcdb94fe3473b73117ea9a6c1ba4ea
Faiz Karim <>
2022-11-25 20:22:10 +0800

    Added checking for registered adapter.

--- lib/static_data.dart
+++ lib/static_data.dart
@@ -12,32 +12,84 @@ export 'constants/constants.dart';
 export 'models/models.dart';
 
 Future<void> initialiseAdapter({required String resourcesHash}) async {
-  registerAdapter(CarerGetStaticDataResponseAdapter());
-  registerAdapter(CustomerGetStaticDataResponseAdapter());
-  registerAdapter(CountryStateAdapter());
-  registerAdapter(PostcodeAdapter());
-  registerAdapter(NationalityAdapter());
-  registerAdapter(BankModelAdapter());
-  registerAdapter(RaceAdapter());
-  registerAdapter(LanguageAdapter());
-  registerAdapter(ReligionAdapter());
-  registerAdapter(MaritalStatusAdapter());
-  registerAdapter(OccupationAdapter());
-  registerAdapter(CarerTypeAdapter());
-  registerAdapter(RateHourAdapter());
-  registerAdapter(RateExtraAdapter());
-  registerAdapter(RateChildrenAdapter());
-  registerAdapter(CarerReviewQuestionAdapter());
-  registerAdapter(BookingStatusAdapter());
-  registerAdapter(JobStatusAdapter());
-  registerAdapter(CarerApplicationStatusAdapter());
-  registerAdapter(InstitutionAdapter());
-  registerAdapter(CarerHighestEducationAdapter());
-  registerAdapter(CarerEducationStatusAdapter());
-  registerAdapter(CarerWorkStatusAdapter());
-  registerAdapter(CarerEmploymentStatusAdapter());
-  registerAdapter(CarerBreadwinnerStatusAdapter());
-  registerAdapter(CarerIncomeCategoryAdapter());
+  if (!Hive.isAdapterRegistered(0)) {
+    registerAdapter(CountryStateAdapter());
+  }
+  if (!Hive.isAdapterRegistered(1)) {
+    registerAdapter(PostcodeAdapter());
+  }
+  if (!Hive.isAdapterRegistered(3)) {
+    registerAdapter(RaceAdapter());
+  }
+  if (!Hive.isAdapterRegistered(4)) {
+    registerAdapter(LanguageAdapter());
+  }
+  if (!Hive.isAdapterRegistered(5)) {
+    registerAdapter(ReligionAdapter());
+  }
+  if (!Hive.isAdapterRegistered(6)) {
+    registerAdapter(MaritalStatusAdapter());
+  }
+  if (!Hive.isAdapterRegistered(7)) {
+    registerAdapter(OccupationAdapter());
+  }
+  if (!Hive.isAdapterRegistered(8)) {
+    registerAdapter(RateHourAdapter());
+  }
+  if (!Hive.isAdapterRegistered(9)) {
+    registerAdapter(RateExtraAdapter());
+  }
+  if (!Hive.isAdapterRegistered(10)) {
+    registerAdapter(RateChildrenAdapter());
+  }
+  if (!Hive.isAdapterRegistered(11)) {
+    registerAdapter(CarerReviewQuestionAdapter());
+  }
+  if (!Hive.isAdapterRegistered(12)) {
+    registerAdapter(BookingStatusAdapter());
+  }
+  if (!Hive.isAdapterRegistered(13)) {
+    registerAdapter(JobStatusAdapter());
+  }
+  if (!Hive.isAdapterRegistered(14)) {
+    registerAdapter(CarerApplicationStatusAdapter());
+  }
+  if (!Hive.isAdapterRegistered(15)) {
+    registerAdapter(BankModelAdapter());
+  }
+  if (!Hive.isAdapterRegistered(16)) {
+    registerAdapter(CarerTypeAdapter());
+  }
+  if (!Hive.isAdapterRegistered(17)) {
+    registerAdapter(NationalityAdapter());
+  }
+  if (!Hive.isAdapterRegistered(18)) {
+    registerAdapter(CarerGetStaticDataResponseAdapter());
+  }
+  if (!Hive.isAdapterRegistered(19)) {
+    registerAdapter(CustomerGetStaticDataResponseAdapter());
+  }
+  if (!Hive.isAdapterRegistered(20)) {
+    registerAdapter(InstitutionAdapter());
+  }
+  if (!Hive.isAdapterRegistered(21)) {
+    registerAdapter(CarerHighestEducationAdapter());
+  }
+  if (!Hive.isAdapterRegistered(22)) {
+    registerAdapter(CarerEducationStatusAdapter());
+  }
+  if (!Hive.isAdapterRegistered(23)) {
+    registerAdapter(CarerWorkStatusAdapter());
+  }
+  if (!Hive.isAdapterRegistered(24)) {
+    registerAdapter(CarerEmploymentStatusAdapter());
+  }
+  if (!Hive.isAdapterRegistered(25)) {
+    registerAdapter(CarerBreadwinnerStatusAdapter());
+  }
+  if (!Hive.isAdapterRegistered(26)) {
+    registerAdapter(CarerIncomeCategoryAdapter());
+  }
 }
 
 void registerAdapter<T extends Object>(TypeAdapter<T> adapter) {
